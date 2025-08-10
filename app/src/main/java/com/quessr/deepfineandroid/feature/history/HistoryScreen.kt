a61f8adf9773f3cbba8937f1b3c093261b0a3156
quessr <heosera76@gmail.com>
2025-08-10 17:54:28 +0900

    feat: History 화면 UI 구현

---
+++ app/src/main/java/com/quessr/deepfineandroid/feature/history/HistoryScreen.kt
@@ -0,0 +1,104 @@
+package com.quessr.deepfineandroid.feature.history
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import androidx.wear.compose.material.Text
+import com.quessr.deepfineandroid.core.ui.theme.bgGrey
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistoryScreen(vm: HistoryViewModel = hiltViewModel(), onGoTodo: () -> Unit) {
+    val state by vm.state.collectAsState()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = onGoTodo) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                title = { Text("History", color = MaterialTheme.colorScheme.onSurface) },
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(bgGrey),
+            contentPadding = PaddingValues(vertical = 8.dp)
+        ) {
+            items(items = state.list,
+                key = { it.id }) { history ->
+                HistoryRow(
+                    content = history.content,
+                    createdAt = history.createdAt.toString(),
+                    completedAt = history.completedAt.toString()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun HistoryRow(
+    content: String,
+    createdAt: String,
+    completedAt: String
+) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+        Text(text = content, color = MaterialTheme.colorScheme.onSurface)
+
+        Spacer(Modifier.height(4.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "등록일: $createdAt", color = MaterialTheme.colorScheme.onSurface)
+            Text(text = "완료일: $completedAt", color = MaterialTheme.colorScheme.onSurface)
+        }
+    }
+}
