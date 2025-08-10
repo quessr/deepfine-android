bf134bb929916d33a819a0c6c690170e0fd9ec02
quessr <heosera76@gmail.com>
2025-08-10 13:01:59 +0900

    refactor: ui/theme 폴더를 core로 이동

---
+++ app/src/main/java/com/quessr/deepfineandroid/core/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.quessr.deepfineandroid.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val bgGrey = Color(0xFFF2F2F4)
+val checked = Color(0xFF2735AE)
