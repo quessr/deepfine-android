dc568048c947e7d4f99f4730e723fd308ee6afb5
quessr <heosera76@gmail.com>
2025-08-10 13:16:26 +0900

    feat: MainActivity에 Todo 화면 네비게이션 연결

---
+++ app/src/main/java/com/quessr/deepfineandroid/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.quessr.deepfineandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.quessr.deepfineandroid.core.ui.theme.DeepfineAndroidTheme
+import com.quessr.deepfineandroid.feature.todo.TodoScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DeepfineAndroidTheme {
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "todo"
+                ) {
+                    composable("todo") {
+                        TodoScreen(
+                            onGoHistory = {navController.navigate("history")}
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
